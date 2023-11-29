1134ed4c3e50d6b833828d98054cc300b705d916
Nguyễn Văn Khoa <khoaqwe1122@gmail.com>
2023-11-29 16:39:26 +0700

    fix

--- fashion_shop/src/views/home/PageHome.vue
+++ fashion_shop/src/views/home/PageHome.vue
@@ -9,17 +9,24 @@
         <section id="gif">
             <div class="container">
                 <div class="list-gif">
-                    <div class="item"><img src="https://i.pinimg.com/564x/d3/f7/74/d3f7742fce9a68304a3ebd04b2fb3580.jpg" alt=""><a href="#">Khuyến mãi Grab this convenient and
+                    <div class="item"><img src="https://i.pinimg.com/564x/d3/f7/74/d3f7742fce9a68304a3ebd04b2fb3580.jpg"
+                            alt=""><a href="#">Khuyến mãi Grab this convenient and
                             free menu with social icons template that can easily work for your page’s header section.
                             There’s also a search bar and a dropdown for you to benefit from.</a></div>
-                    <div class="item"><img src="https://i.pinimg.com/564x/af/9f/f5/af9ff5fde03cea0598da4e6ffc353623.jpg" alt=""><a href="#">Khuyến mãi</a></div>
-                    <div class="item"><img src="https://i.pinimg.com/564x/9c/8d/a6/9c8da6f5c47b6edc0696a41e59e2c174.jpg" alt=""><a href="#">Khuyến mãi</a></div>
-                    <div class="item"><img src="https://i.pinimg.com/564x/9c/8d/a6/9c8da6f5c47b6edc0696a41e59e2c174.jpg" alt=""><a href="#">Khuyến mãi</a></div>
-                    <div class="item"><img src="https://i.pinimg.com/564x/9c/8d/a6/9c8da6f5c47b6edc0696a41e59e2c174.jpg" alt=""><a href="#">Khuyến mãi</a></div>
-                    <div class="item"><img src="https://i.pinimg.com/564x/9c/8d/a6/9c8da6f5c47b6edc0696a41e59e2c174.jpg" alt=""><a href="#">Khuyến mãi</a></div>
-                    <div class="item"><img src="https://i.pinimg.com/564x/9c/8d/a6/9c8da6f5c47b6edc0696a41e59e2c174.jpg" alt=""><a href="#">Khuyến mãi</a></div>
-                    <div class="item"><img src="https://i.pinimg.com/564x/9c/8d/a6/9c8da6f5c47b6edc0696a41e59e2c174.jpg" alt=""><a href="#">Khuyến mãi</a></div>
-
+                    <div class="item"><img src="https://i.pinimg.com/564x/af/9f/f5/af9ff5fde03cea0598da4e6ffc353623.jpg"
+                            alt=""><a href="#">Khuyến mãi</a></div>
+                    <div class="item"><img src="https://i.pinimg.com/564x/9c/8d/a6/9c8da6f5c47b6edc0696a41e59e2c174.jpg"
+                            alt=""><a href="#">Khuyến mãi</a></div>
+                    <div class="item"><img src="https://i.pinimg.com/564x/9c/8d/a6/9c8da6f5c47b6edc0696a41e59e2c174.jpg"
+                            alt=""><a href="#">Khuyến mãi</a></div>
+                    <div class="item"><img src="https://i.pinimg.com/564x/9c/8d/a6/9c8da6f5c47b6edc0696a41e59e2c174.jpg"
+                            alt=""><a href="#">Khuyến mãi</a></div>
+                    <div class="item"><img src="https://i.pinimg.com/564x/9c/8d/a6/9c8da6f5c47b6edc0696a41e59e2c174.jpg"
+                            alt=""><a href="#">Khuyến mãi</a></div>
+                    <div class="item"><img src="https://i.pinimg.com/564x/9c/8d/a6/9c8da6f5c47b6edc0696a41e59e2c174.jpg"
+                            alt=""><a href="#">Khuyến mãi</a></div>
+                    <div class="item"><img src="https://i.pinimg.com/564x/9c/8d/a6/9c8da6f5c47b6edc0696a41e59e2c174.jpg"
+                            alt=""><a href="#">Khuyến mãi</a></div>
                 </div>
             </div>
         </section>
@@ -32,8 +39,21 @@
                     <span class="more">Xem thêm &ensp; <i class="fa-solid fa-angles-right" style="color: blue;"></i></span>
                 </div>
                 <div class="list-product-flash-sale">
-                    <Product :data="item"></Product>
+                    <vueper-slides autoplay class="no-shadow" id="slide-flash-sale" :visible-slides="5" :slide-ratio="1 / 4"
+                        :dragging-distance="70" :fixed-height="true" :touchable="true" :bullets="false" slide-multiple>
+                        <vueper-slide v-for="(item, key) in listProduct" :key="key">
+                            <template #content>
+                                <Product :data="item" :arrows="false"></Product>
+                            </template>
+                        </vueper-slide>
+                    </vueper-slides>
                 </div>
+            </div>
+        </section>
+        <section id="top-selling-item" class="container">
+            <div class="title-section">
+                <h4>SẢN PHẨM BÁN CHẠY</h4>
+                <span class="more">Xem thêm &ensp; <i class="fa-solid fa-angles-right" style="color: blue;"></i></span>
             </div>
         </section>
         <section id="top-selling-item" class="container">
@@ -111,12 +131,12 @@ export default {
             item: {
                 name: 'product_1',
                 imgs: [
-                    {name:'xanh',img:'https://i.pinimg.com/564x/11/7b/ae/117bae9845cd9758c8323639a28b663e.jpg'},
-                    {name:'Dổ',img:'https://i.pinimg.com/564x/67/af/b3/67afb335f3e0cf5ba519ac29b19ee383.jpg'},
-                    {name:'xanh',img:'https://i.pinimg.com/564x/12/52/f7/1252f7650a473bbbef328d1b0a4d72ce.jpg'},
-                    {name:'vang',img:'https://i.pinimg.com/564x/bb/3b/30/bb3b3033c79b78c5582acb274bacee72.jpg'},
-                    {name:'xanh',img:'https://i.pinimg.com/736x/b3/77/a5/b377a5c3bd632c04f40caf17661fd655.jpg'},
-                    {name:'xanh',img:'https://i.pinimg.com/564x/b7/c3/92/b7c392a35e18cb1133a50006dda4356d.jpg'},
+                    { name: 'xanh', img: 'https://i.pinimg.com/564x/11/7b/ae/117bae9845cd9758c8323639a28b663e.jpg' },
+                    { name: 'Dổ', img: 'https://i.pinimg.com/564x/67/af/b3/67afb335f3e0cf5ba519ac29b19ee383.jpg' },
+                    { name: 'xanh', img: 'https://i.pinimg.com/564x/12/52/f7/1252f7650a473bbbef328d1b0a4d72ce.jpg' },
+                    { name: 'vang', img: 'https://i.pinimg.com/564x/bb/3b/30/bb3b3033c79b78c5582acb274bacee72.jpg' },
+                    { name: 'xanh', img: 'https://i.pinimg.com/736x/b3/77/a5/b377a5c3bd632c04f40caf17661fd655.jpg' },
+                    { name: 'xanh', img: 'https://i.pinimg.com/564x/b7/c3/92/b7c392a35e18cb1133a50006dda4356d.jpg' },
                 ],
                 vote: '3.2',
                 quantitySold: '333',
@@ -127,44 +147,101 @@ export default {
             listProduct: [
                 {
                     name: 'product_1',
-                    img: 'https://cdn.diemnhangroup.com/seoulcenter/2022/11/gai-xinh-20.jpg',
+                    imgs: [
+                        { name: 'xanh', img: 'https://i.pinimg.com/564x/11/7b/ae/117bae9845cd9758c8323639a28b663e.jpg' },
+                        { name: 'Dổ', img: 'https://i.pinimg.com/564x/67/af/b3/67afb335f3e0cf5ba519ac29b19ee383.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/564x/12/52/f7/1252f7650a473bbbef328d1b0a4d72ce.jpg' },
+                        { name: 'vang', img: 'https://i.pinimg.com/564x/bb/3b/30/bb3b3033c79b78c5582acb274bacee72.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/736x/b3/77/a5/b377a5c3bd632c04f40caf17661fd655.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/564x/b7/c3/92/b7c392a35e18cb1133a50006dda4356d.jpg' },
+                    ],
                     vote: '3.2',
                     quantitySold: '333',
-                    sale: '10%',
+                    sale: '10',
                     price: '203222',
+                    imgGift: 'https://cdn.diemnhangroup.com/seoulcenter/2022/11/gai-xinh-17.jpg'
                 },
                 {
-                    name: 'product_1',
-                    img: 'https://cdn.diemnhangroup.com/seoulcenter/2022/11/gai-xinh-20.jpg',
+                    name: 'product_2',
+                    imgs: [
+                        { name: 'xanh', img: 'https://www.pinterest.com/pin/582582901826138192/' },
+                        { name: 'Dổ', img: 'https://i.pinimg.com/564x/67/af/b3/67afb335f3e0cf5ba519ac29b19ee383.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/564x/12/52/f7/1252f7650a473bbbef328d1b0a4d72ce.jpg' },
+                        { name: 'vang', img: 'https://i.pinimg.com/564x/bb/3b/30/bb3b3033c79b78c5582acb274bacee72.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/736x/b3/77/a5/b377a5c3bd632c04f40caf17661fd655.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/564x/b7/c3/92/b7c392a35e18cb1133a50006dda4356d.jpg' },
+                    ],
                     vote: '3.2',
                     quantitySold: '333',
-                    sale: '10%',
+                    sale: '10',
                     price: '203222',
+                    imgGift: 'https://cdn.diemnhangroup.com/seoulcenter/2022/11/gai-xinh-17.jpg'
                 },
                 {
-                    name: 'product_1',
-                    img: 'https://cdn.diemnhangroup.com/seoulcenter/2022/11/gai-xinh-20.jpg',
+                    name: 'product_3',
+                    imgs: [
+                        { name: 'xanh', img: 'https://www.pinterest.com/pin/582582901816088821/' },
+                        { name: 'Dổ', img: 'https://i.pinimg.com/564x/67/af/b3/67afb335f3e0cf5ba519ac29b19ee383.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/564x/12/52/f7/1252f7650a473bbbef328d1b0a4d72ce.jpg' },
+                        { name: 'vang', img: 'https://i.pinimg.com/564x/bb/3b/30/bb3b3033c79b78c5582acb274bacee72.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/736x/b3/77/a5/b377a5c3bd632c04f40caf17661fd655.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/564x/b7/c3/92/b7c392a35e18cb1133a50006dda4356d.jpg' },
+                    ],
                     vote: '3.2',
                     quantitySold: '333',
-                    sale: '10%',
+                    sale: '10',
                     price: '203222',
+                    imgGift: 'https://cdn.diemnhangroup.com/seoulcenter/2022/11/gai-xinh-17.jpg'
                 },
                 {
-                    name: 'product_1',
-                    img: 'https://cdn.diemnhangroup.com/seoulcenter/2022/11/gai-xinh-20.jpg',
+                    name: 'product_4',
+                    imgs: [
+                        { name: 'xanh', img: 'https://i.pinimg.com/236x/88/25/dd/8825dd195accfe22207d6b235d7d5829.jpg' },
+                        { name: 'Dổ', img: 'https://i.pinimg.com/564x/67/af/b3/67afb335f3e0cf5ba519ac29b19ee383.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/564x/12/52/f7/1252f7650a473bbbef328d1b0a4d72ce.jpg' },
+                        { name: 'vang', img: 'https://i.pinimg.com/564x/bb/3b/30/bb3b3033c79b78c5582acb274bacee72.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/736x/b3/77/a5/b377a5c3bd632c04f40caf17661fd655.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/564x/b7/c3/92/b7c392a35e18cb1133a50006dda4356d.jpg' },
+                    ],
                     vote: '3.2',
                     quantitySold: '333',
-                    sale: '10%',
+                    sale: '10',
                     price: '203222',
+                    imgGift: 'https://cdn.diemnhangroup.com/seoulcenter/2022/11/gai-xinh-17.jpg'
                 },
                 {
-                    name: 'product_1',
-                    img: 'https://cdn.diemnhangroup.com/seoulcenter/2022/11/gai-xinh-20.jpg',
+                    name: 'product_5',
+                    imgs: [
+                        { name: 'xanh', img: 'https://i.pinimg.com/236x/5b/f4/29/5bf429c2ebaf6107bba20b94bfb0bbab.jpg' },
+                        { name: 'Dổ', img: 'https://i.pinimg.com/564x/67/af/b3/67afb335f3e0cf5ba519ac29b19ee383.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/564x/12/52/f7/1252f7650a473bbbef328d1b0a4d72ce.jpg' },
+                        { name: 'vang', img: 'https://i.pinimg.com/564x/bb/3b/30/bb3b3033c79b78c5582acb274bacee72.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/736x/b3/77/a5/b377a5c3bd632c04f40caf17661fd655.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/564x/b7/c3/92/b7c392a35e18cb1133a50006dda4356d.jpg' },
+                    ],
                     vote: '3.2',
                     quantitySold: '333',
-                    sale: '10%',
+                    sale: '10',
                     price: '203222',
+                    imgGift: 'https://cdn.diemnhangroup.com/seoulcenter/2022/11/gai-xinh-17.jpg'
                 },
+                {
+                    name: 'product_6',
+                    imgs: [
+                        { name: 'xanh', img: 'https://i.pinimg.com/564x/24/c9/e1/24c9e1b0856037f442528fe3b3373dca.jpg' },
+                        { name: 'Dổ', img: 'https://i.pinimg.com/564x/67/af/b3/67afb335f3e0cf5ba519ac29b19ee383.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/564x/12/52/f7/1252f7650a473bbbef328d1b0a4d72ce.jpg' },
+                        { name: 'vang', img: 'https://i.pinimg.com/564x/bb/3b/30/bb3b3033c79b78c5582acb274bacee72.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/736x/b3/77/a5/b377a5c3bd632c04f40caf17661fd655.jpg' },
+                        { name: 'xanh', img: 'https://i.pinimg.com/564x/b7/c3/92/b7c392a35e18cb1133a50006dda4356d.jpg' },
+                    ],
+                    vote: '3.2',
+                    quantitySold: '333',
+                    sale: '10',
+                    price: '203222',
+                    imgGift: 'https://cdn.diemnhangroup.com/seoulcenter/2022/11/gai-xinh-17.jpg'
+                },
+
             ]
         };
     },
@@ -178,7 +255,6 @@ export default {
         // được sử dụng để định nghĩa các thuộc tính tính toán
     },
     updated() {
-
     },
     unmounted() {
 
@@ -190,6 +266,10 @@ export default {
 </script>
   
 <style scoped>
+.list-product-flash-sale {
+    padding: 0 3rem;
+}
+
 .vueperslides--fixed-height {
     height: 35rem;
 }
