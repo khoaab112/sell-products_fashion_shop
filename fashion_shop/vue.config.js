48804a69b7b4e85d7d66f32777202b1b37edc3c0
Nguyễn Văn Khoa <khoaqwe1122@gmail.com>
2024-03-09 15:36:31 +0700

    main - update file config

--- fashion_shop/vue.config.js
+++ fashion_shop/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+var port = process.env.APP_ENV == 'local' ? process.env.PORT_LO : process.env.PORT_PRO
 module.exports = defineConfig({
+    devServer: {
+        port: port,
+    },
     transpileDependencies: true
 })
